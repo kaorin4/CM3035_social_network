2fb3de142913551c22782960e99675c5f80452ce
Kaori Murakami <kaori.murakami4@gmail.com>
2021-09-12 17:38:33 -0500

    remvoe file

--- socialnetwork_project/friend/utils.py
+++
@@ -1,7 +0,0 @@
-from friend.models import FriendRequest
-
-def get_friend_request(sender, receiver):
-    try:
-        return FriendRequest.objects.get(sender=sender, receiver=receiver, is_active=True)
-    except FriendRequest.DoesNotExist:
-        return False
